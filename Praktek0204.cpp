f8349dbe3edd1a92e253bd0d506da85122abbb56
noryunitaris <yunitarisnaida@gmail.com>
2017-10-22 08:59:30 -0700

    Luas 1 box parket

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -5,4 +5,8 @@ int main(){
 	double luasrumah = 42;
 	cout << "Luas Rumah			: " << luasrumah << " m2" <<endl;
 	
+	double lantaiparket = 193*1215;
+	double luas1boxparket = lantaiparket*8/1000000;
+	cout << "Luas 1 box parket		: " << luas1boxparket << " m2"<<endl;
+	
 }
